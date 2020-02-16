072f39704df324d3b55f14cd905a74647d3076d6
michseeto <mich05@live.com>
2020-02-16 18:57:19 +1100

    Frontend Repo

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const drawerWidth = 240;
+export const url = 'https://slackr-unsw.herokuapp.com/';
+// export const url = 'http://localhost:' + window.BACKEND_PORT;
+export const PERMISSION_IDS = {
+    OWNER: 1,
+    ADMIN: 2,
+    MEMBER: 3,
+}
+export const PAGINATION_SIZE = 50;
+export const SLICE_SIZE = 10;
